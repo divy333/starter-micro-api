b0ee64a6e486fdb8fb44a874c4822f63ee79667a
divy333 <divyanandyadav25051996@gmail.com>
2023-11-03 17:18:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,8 @@ http.createServer(function (req, res) {
         //res.answer = 'Hello';
         //res.write
         //var answer = "Hello There!";
-        res.write(Math.random()*10069);
+        var token = Math.random()*10069;
+        res.write(token.toString());
         return res.end();
     }
     //res.write("<html> <head>server Response</head><body><h1> This page was render direcly from the server <p>Hello there welcome to my website</p></h1></body></html>");

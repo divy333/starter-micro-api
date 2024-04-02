e3baf675fc32b103d3bfe5cb8f517b1098ea0e2f
divy333 <divyanandyadav25051996@gmail.com>
2024-04-02 21:06:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ http.createServer(function (req, res) {
     else if (req.url === '/ask')
     {
         console.log("Inside ask");
-        console.log(req);
+        console.log(req.data);
         //res.write("Hello");
         //res.answer = 'Hello';
         //res.write
